d965140ede7e8ed235a79ff46f7ae7262ab566d1
chunmusic <chun_ps@hotmail.com>
2021-01-17 00:53:36 +0900

    add code

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/pongsakorn/ros_object_detection/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/pongsakorn/ros_face_detection/devel;/home/pongsakorn/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/pongsakorn/ros_face_detection/devel;/home/pongsakorn/catkin_ws/devel;/opt/ros/melodic' != '' else []
